92bf39ba9b8c4c18e0ab15a06e82b2e609a410f6
Zoman-ankit <ankit.jha@zomato.com>
2021-08-11 00:58:30 +0530

    fragment lives

--- app/src/main/java/com/bigbrain/expenses/view/MainActivity.kt
+++ app/src/main/java/com/bigbrain/expenses/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bigbrain.expenses
+package com.bigbrain.expenses.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bigbrain.expenses.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
