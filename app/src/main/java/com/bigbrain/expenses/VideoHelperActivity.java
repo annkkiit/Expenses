bacbbcf4bc5bde291eee486cebfe2e384a648571
Zoman-ankit <ankit.jha@zomato.com>
2021-08-13 21:39:41 +0530

    final push

--- app/src/main/java/com/bigbrain/expenses/VideoHelperActivity.java
+++ app/src/main/java/com/bigbrain/expenses/VideoHelperActivity.java
@@ -2,7 +2,14 @@ package com.bigbrain.expenses;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import org.jitsi.meet.sdk.JitsiMeet;
 import org.jitsi.meet.sdk.JitsiMeetActivity;
@@ -13,11 +20,14 @@ import java.net.URL;
 
 public class VideoHelperActivity extends AppCompatActivity {
 
+    TextInputLayout t ;
+    MaterialButton btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_helper);
-
+        t = findViewById(R.id.et_title_view2);
+        btn = findViewById(R.id.btnsubmit);
         // Initialize default options for Jitsi Meet conferences.
         URL serverURL;
         try {
@@ -34,18 +44,27 @@ public class VideoHelperActivity extends AppCompatActivity {
 
         JitsiMeet.setDefaultConferenceOptions(defaultOptions);
 
-        String text = "Expert consulting";
 
-        if (text.length() > 0) {
-            // Build options object for joining the conference. The SDK will merge the default
-            // one we set earlier and this one when joining.
-            JitsiMeetConferenceOptions options
-                    = new JitsiMeetConferenceOptions.Builder()
-                    .setRoom(text)
-                    .build();
-            // Launch the new activity with the given options. The launch() method takes care
-            // of creating the required Intent and passing the options.
-            JitsiMeetActivity.launch(this, options);
-        }
+        btn.setOnClickListener(v -> {
+            String text = t.getEditText().toString();
+            if(!text.isEmpty()) {
+                if (text.length() > 0) {
+                    // Build options object for joining the conference. The SDK will merge the default
+                    // one we set earlier and this one when joining.
+                    JitsiMeetConferenceOptions options
+                            = new JitsiMeetConferenceOptions.Builder()
+                            .setRoom(text)
+                            .build();
+                    // Launch the new activity with the given options. The launch() method takes care
+                    // of creating the required Intent and passing the options.
+                    JitsiMeetActivity.launch(this, options);
+                }
+            } else {
+                Toast.makeText(this, "PLease fill your concern", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
+
 }
