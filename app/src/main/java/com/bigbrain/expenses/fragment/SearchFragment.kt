7ee1650dd6c86c31f2139c3cefb89d672881a593
Zoman-ankit <ankit.jha@zomato.com>
2021-08-13 16:19:43 +0530

    jisi live, new onboarding

--- app/src/main/java/com/bigbrain/expenses/fragment/SearchFragment.kt
+++ app/src/main/java/com/bigbrain/expenses/fragment/SearchFragment.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import com.bigbrain.expenses.R
+import org.jitsi.meet.sdk.JitsiMeet
+import org.jitsi.meet.sdk.JitsiMeetActivity
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions
+import org.webrtc.ContextUtils.getApplicationContext
+import java.net.MalformedURLException
+import java.net.URL
 
 /**
  * A simple [Fragment] subclass.
@@ -20,8 +26,41 @@ class SearchFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val serverURL: URL
+        serverURL = try {
+            URL("https://meet.jit.si")
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+            throw RuntimeException("Invalid server URL!")
+        }
+        val defaultOptions = JitsiMeetConferenceOptions.Builder()
+            .setServerURL(serverURL)
+            .setWelcomePageEnabled(false)
+            .build()
+        JitsiMeet.setDefaultConferenceOptions(defaultOptions)
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.search_menu, menu)
         true
+    }
+
+    fun onjitsi(view: View) {
+
+//        EditText editText = findViewById(R.id.conferenceName);
+        val text = "Expert Consultant"
+
+        if (text.length > 0) {
+            // Build options object for joining the conference. The SDK will merge the default
+            // one we set earlier and this one when joining.
+            val options = JitsiMeetConferenceOptions.Builder()
+                .setRoom(text)
+                .build()
+            // Launch the new activity with the given options. The launch() method takes care
+            // of creating the required Intent and passing the options.
+            JitsiMeetActivity.launch(getApplicationContext(), options)
+        }
     }
 }
